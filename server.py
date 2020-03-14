8efb3798226e0debd34edcfd9fee5c25fb00d0ce
Zen Tang <zen@wayve.ai>
2020-03-14 17:37:11 -0500

    minor

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/game/<int:game_id>')
+def find_game(game_id):
+    msg = f"Game {game_id} doesn't exist yet!!"
+    return msg
+    
 @app.route('/state')
 def hello_world():
     return 'Hello, World!'
