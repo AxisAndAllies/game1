7c1cc4a179dba6b161c2cacf7177252508d46a3e
Zen Tang <zen@wayve.ai>
2020-03-14 18:16:13 -0500

    minor

--- test_game.py
+++ test_game.py
@@ -1,10 +1,23 @@
 from game import Game
 
+
 def test_attack():
     g = Game()
+
     UNITS = 3
     FROM_TERRITORY = 1
-    TO_TERRITORY = 2
+    TO_TERRITORY = 3        # enemy territory
+
+    original_units_a = g.pieces[FROM_TERRITORY]['strength']
+    original_units_b = g.pieces[TO_TERRITORY]['strength']
+
+    g.move(UNITS, FROM_TERRITORY, TO_TERRITORY)
+
+    new_units_a = g.pieces[FROM_TERRITORY]['strength']
+    new_units_b = g.pieces[TO_TERRITORY]['strength']
+
+    print('Original units:', original_units_a, original_units_b)
+    print('New units:', new_units_a, new_units_b)
 
 
 
@@ -18,22 +31,20 @@ def test_move():
     original_units_a = g.pieces[FROM_TERRITORY]['strength']
     original_units_b = g.pieces[TO_TERRITORY]['strength']
 
-    g.move(
-        UNITS,
-        FROM_TERRITORY,
-        TO_TERRITORY
-    )
+    g.move(UNITS, FROM_TERRITORY, TO_TERRITORY)
 
     new_units_a = g.pieces[FROM_TERRITORY]['strength']
     new_units_b = g.pieces[TO_TERRITORY]['strength']
-    
+
     print('Original units:', original_units_a, original_units_b)
     print('New units:', new_units_a, new_units_b)
 
     if original_units_a + original_units_b != new_units_a + new_units_b:
         raise ValueError('Units must be conserved')
-    
+
     if original_units_a - new_units_a != UNITS:
         raise ValueError("Didn't move {} units".format(UNITS))
 
+
 test_move()
+test_attack()
