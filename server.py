db7dbeda9b4d225ce2c99ff99ae7603b563a3a91
Zen Tang <zen@wayve.ai>
2020-03-14 18:26:04 -0500

    minor

--- server.py
+++ server.py
@@ -7,14 +7,17 @@ only_game = Game()
 # def render(risk: Risk):
 #     return risk.state
 
+
 @app.route('/game/<int:game_id>', methods=["GET"])
 def find_game(game_id):
     msg = f"Game {game_id} doesn't exist yet!!"
     return msg
-    
+
+
 @app.route('/state')
 def game_state():
     return only_game.state
+
 
 @app.route('/move', methods=["POST"])
 def move():
@@ -25,10 +28,12 @@ def move():
     only_game.move(num, node_index_from, node_index_to)
     return 'moved.'
 
+
 @app.route('/end_turn', methods=["POST"])
 def end_turn():
     only_game.next_turn()
     return 'ended.'
+
 
 @app.route('/reinforce', methods=["POST"])
 def reinforce():
