3d576fe0db5a04a694dbfab3411f54a6631542ae
cktang88 <kwuang@tryramp.com>
2020-03-14 18:13:57 -0500

    x

--- server.py
+++ server.py
@@ -8,11 +8,24 @@ only_game = Risk()
 def render(risk: Risk):
     return risk.state
 
-@app.route('/game/<int:game_id>')
+@app.route('/game/<int:game_id>', methods=["GET"])
 def find_game(game_id):
     msg = f"Game {game_id} doesn't exist yet!!"
     return msg
     
 @app.route('/state')
-def hello_world():
+def game_state():
     return 'Hello, World!'
+
+@app.route('/move', methods=["POST"])
+def move():
+
+    return 'moved.'
+
+@app.route('/end_turn', methods=["POST"])
+def end_turn():
+    return 'ended.'
+
+@app.route('/reinforce', methods=["POST"])
+def reinforce():
+    return 'reinforced.'
