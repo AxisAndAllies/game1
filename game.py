ae5637e3908a1532d859c4115330ad4ac2b60b41
cktang88 <kwuang@tryramp.com>
2020-03-14 17:32:16 -0500

    xp

---
+++ game.py
@@ -0,0 +1,20 @@
+nodes = {
+    1: [2,3],
+    2:[1,3],
+    3: [1,2]
+}
+
+pieces = {
+    1: {
+        "owner": 'a',
+        "strength": 22
+    },
+    2: {
+        "owner": 'a',
+        "strength": 30
+    },
+    3: {
+        "owner": 'b',
+        "strength": 25
+    }
+}
